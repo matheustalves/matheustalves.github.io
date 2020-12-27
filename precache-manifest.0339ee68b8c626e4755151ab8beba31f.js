7c3cddeb97a286fe575b9f5469804a8f9f7e8bbb
Matheus <matheustalves@outlook.com>
2020-12-27 13:21:51 -0300

    Updates

---
+++ precache-manifest.0339ee68b8c626e4755151ab8beba31f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdfb484964b6aaf19d13adfa18ec975e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41ee7bb500037c047771",
+    "url": "/static/css/main.12105261.chunk.css"
+  },
+  {
+    "revision": "d57960d1d79e257e7485",
+    "url": "/static/js/2.31f41cb6.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.31f41cb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41ee7bb500037c047771",
+    "url": "/static/js/main.44980636.chunk.js"
+  },
+  {
+    "revision": "19b015b11338ae68137f",
+    "url": "/static/js/runtime-main.6f29efd1.js"
+  }
+]);
